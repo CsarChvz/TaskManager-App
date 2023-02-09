da74d7b83e6b789aed80bda2c1c4ae8eb696e57f
CsarChvz <79390377+CsarChvz@users.noreply.github.com>
2023-02-08 22:12:57 -0600

    Cambios

---
+++ index.ts
@@ -0,0 +1,34 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+async function main() {
+  await prisma.$connect();
+
+  await prisma.tasks.create({
+    data: {
+      title: "Testing Node.js",
+      description: "Create a Node.js tasks application",
+    },
+  });
+
+  await prisma.tasks.create({
+    data: {
+      title: "Learning Java",
+      description: "Creating some Java APIs",
+    },
+  });
+
+  await prisma.tasks.create({
+    data: {
+      title: "Testing Out Flask APIs",
+      description: "Set up some Flask APIs",
+    },
+  });
+
+  const tasks = await prisma.tasks.findMany();
+
+  console.dir(tasks, { depth: Infinity });
+}
+
+main()
+  .catch(console.error)
+  .finally(() => prisma.$disconnect());

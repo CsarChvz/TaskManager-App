b45dee535a222524bdc742163a0747439f9aa743
CsarChvz <79390377+CsarChvz@users.noreply.github.com>
2023-02-13 07:53:44 -0600

    O

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ const express = require("express");
 const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
-
+let bodyParser = require("body-parser");
 const app = express();
 const port = process.env.port || 3000;
 
 // Setamos los valores para el servidor para que nos devuelva un JSON
 app.set(express.json());
-
 app.post("/users", (req, res) => {
-  console.log("asdf");
+  console.log("Body", req.body);
+  res.send("Andres");
 });
 
 app.listen(port, () => {

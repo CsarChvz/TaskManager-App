45cbfe7981476607c14dfb49e9747a28313edb21
CsarChvz <79390377+CsarChvz@users.noreply.github.com>
2023-02-13 18:12:43 -0600

    Update and delete

--- src/index.js
+++ src/index.js
@@ -92,6 +92,7 @@ app.get("/tasks/:id", async (req, res) => {
 });
 
 // @@@ PATCH - End points
+
 app.patch("/users/:id", async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ["name", "email", "password"];
@@ -116,6 +117,78 @@ app.patch("/users/:id", async (req, res) => {
     res.status(200).json(usersData);
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+});
+
+app.patch("/tasks/:id", async (req, res) => {
+  // Los campos que se agregaron para actualizar
+  const updates = Object.keys(req.body);
+  // Estos son los campos que se permiten actualizar
+  const allowedUpdates = ["description", "completed"];
+
+  // Funcion que checa si es que se puede hacer la operación
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.send(400).send({
+      error: "Invalid updates",
+    });
+  }
+
+  try {
+    let _id = req.params.id.toString();
+    const task = await prisma.tasks.update({
+      where: {
+        id: _id,
+      },
+      data: {
+        ...req.body,
+      },
+    });
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.status(200).send(task);
+  } catch (error) {
+    return res.status(400).send({ error: Error(error) });
+  }
+});
+
+// @@@ DELETE - End points
+app.delete("/users/id", async (req, res) => {
+  try {
+    let _id = req.params.id.toString();
+    let userDeleted = await prisma.user.delete({
+      where: {
+        id: _id,
+      },
+    });
+
+    if (!userDeleted) {
+      return res.status(404).send({ error: "User not found" });
+    }
+    return res.status(200).send(userDeleted);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+app.delete("/tasks/id", async (req, res) => {
+  try {
+    let _id = req.params.id.toString();
+    let userDeleted = await prisma.user.delete({
+      where: {
+        id: _id,
+      },
+    });
+
+    if (!userDeleted) {
+      return res.status(404).send({ error: "User not found" });
+    }
+    return res.status(200).send(userDeleted);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
   }
 });
 

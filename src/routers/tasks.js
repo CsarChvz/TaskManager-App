110c419b41daf0b490bfb7216afe2e1bd8bc0781
César Chávez <79390377+CsarChvz@users.noreply.github.com>
2023-02-16 13:31:41 -0800

    Autenticacón en tareas

--- src/routers/tasks.js
+++ src/routers/tasks.js
@@ -27,21 +27,16 @@ tasks.post("/tasks", auth, async (req, res) => {
   }
 });
 
-tasks.get("/tasks", async (req, res) => {
+tasks.get("/tasks", auth, async (req, res) => {
   try {
-    let tasksData = await prisma.tasks.findMany();
-    res.status(200).json(tasksData);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-tasks.get("/tasks/:id", async (req, res) => {
-  let _id = req.params.id.toString();
-  try {
-    let tasksData = await prisma.tasks.findUniqueOrThrow({
+    let tasksData = await prisma.tasks.findMany({
       where: {
-        id: _id,
+        owner: {
+          id: req.user.id.toString(),
+        },
+      },
+      include: {
+        owner: true,
       },
     });
     res.status(200).json(tasksData);
@@ -50,7 +45,31 @@ tasks.get("/tasks/:id", async (req, res) => {
   }
 });
 
-tasks.patch("/tasks/:id", async (req, res) => {
+tasks.get("/tasks/:id", auth, async (req, res) => {
+  try {
+    let _id = req.params.id.toString();
+    // Get all tasks from a user
+
+    // Para obtener la tarea de un usuario, tenemos que checar que la tarea que se este llamando, pertenezca al usuario que esta haciendo la petición
+    let tasksData = await prisma.tasks.findMany({
+      where: {
+        id: _id,
+        owner: {
+          id: req.user.id.toString(),
+        },
+      },
+
+      include: {
+        owner: true,
+      },
+    });
+    res.status(200).json(tasksData);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+tasks.patch("/tasks/:id", auth, async (req, res) => {
   // Los campos que se agregaron para actualizar
   const updates = Object.keys(req.body);
   // Estos son los campos que se permiten actualizar
@@ -68,39 +87,67 @@ tasks.patch("/tasks/:id", async (req, res) => {
   }
 
   try {
-    let _id = req.params.id.toString();
-    const task = await prisma.tasks.update({
+    let idTask = req.params.id.toString();
+    let idUser = req.user.id.toString();
+    // Nada más tenemos que obtener la task, checamos que el id del usuario que esta haciendo la petición, sea el mismo que el que esta en la tarea
+    let task = await prisma.tasks.findFirstOrThrow({
       where: {
-        id: _id,
-      },
-      data: {
-        ...req.body,
+        id: idTask,
+        owner: {
+          id: idUser,
+        },
       },
     });
+
     if (!task) {
       return res.status(404).send();
+    } else {
+      let taskUpdate = await prisma.tasks.update({
+        where: {
+          id: idTask,
+        },
+        data: {
+          ...req.body,
+        },
+      });
+      res.status(200).json(taskUpdate);
     }
-    res.status(200).send(task);
+
+    if (!task) {
+      return res.status(404).json(error.message);
+    }
   } catch (error) {
-    return res.status(400).send({ error: Error(error) });
+    return res.status(400).json(error.message);
   }
 });
 
-tasks.delete("/tasks/id", async (req, res) => {
+tasks.delete("/tasks/:id", auth, async (req, res) => {
   try {
-    let _id = req.params.id.toString();
-    let userDeleted = await prisma.user.delete({
+    let idTask = req.params.id.toString();
+    let idUser = req.user.id.toString();
+    // Nada más tenemos que obtener la task, checamos que el id del usuario que esta haciendo la petición, sea el mismo que el que esta en la tarea
+    let task = await prisma.tasks.findFirstOrThrow({
       where: {
-        id: _id,
+        id: idTask,
+        owner: {
+          id: idUser,
+        },
       },
     });
 
-    if (!userDeleted) {
-      return res.status(404).send({ error: "User not found" });
+    if (!task) {
+      return res.status(404).send();
+    } else {
+      let taskDelete = await prisma.tasks.delete({
+        where: {
+          id: idTask,
+        },
+      });
+      res.status(200).json();
     }
-    return res.status(200).send(userDeleted);
   } catch (error) {
-    return res.status(500).json({ error: error.message });
+    return res.status(400).json(error.message);
   }
 });
+
 module.exports = tasks;
